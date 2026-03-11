c194cc7780385290c9aa627e7655ad31d2bd6184
Swackyy <68652827+Swackyy@users.noreply.github.com>
2026-03-11 19:10:06 +0000

    Add  "Mixin magic" Javadocs

--- renamer-gradle/src/main/java/net/minecraftforge/renamer/gradle/MixinConfig.java
+++ renamer-gradle/src/main/java/net/minecraftforge/renamer/gradle/MixinConfig.java
@@ -15,22 +15,61 @@ import org.gradle.api.tasks.bundling.Jar;
 
 public interface MixinConfig extends MixinSourceSetConfig {
 	ListProperty<String> getConfigs();
+
+    /**
+     * {@inheritDoc}
+     * Add a Mixin config
+     * @param name path to config file, searches from {@code resources/}
+     */
 	default void config(String name) {
 		getConfigs().add(name);
 	}
+
+    /**
+     * {@inheritDoc}
+     * Add a Mixin config
+     * @param name path to config file, searches from {@code resources/}
+     */
 	default void config(Provider<String> name) {
 		getConfigs().add(name);
 	}
 
+    /**
+     * {@inheritDoc}
+     * Add a source set to Mixin, automatically infers refmap name
+     * @param source source set to add
+     */
 	default MixinSourceSetConfig source(SourceSet source) {
 		return source(source, source.getName().toLowerCase(Locale.ENGLISH));
 	}
+
+    /**
+     * {@inheritDoc}
+     * Add and source set to Mixin and configure it, automatically infers refmap name
+     * @param source source set to add
+     * @param action configuration for the source set
+     */
 	default MixinSourceSetConfig source(SourceSet source, Action<? super MixinSourceSetConfig> action) {
 		return source(source, source.getName().toLowerCase(Locale.ENGLISH), action);
 	}
+
+    /**
+     * {@inheritDoc}
+     * Add a source set to Mixin
+     * @param source source set to add
+     * @param name prefix to the output refmap.json file
+     */
 	default MixinSourceSetConfig source(SourceSet source, String name) {
 		return source(source, name, cfg -> {});
 	}
+
+    /**
+     * {@inheritDoc}
+     * Add and source set to Mixin and configure it
+     * @param source source set to add
+     * @param name prefix to the output refmap.json file
+     * @param action configuration for the source set
+     */
 	MixinSourceSetConfig source(SourceSet source, String name, Action<? super MixinSourceSetConfig> action);
 
 	void run(Object runConfig);
--- renamer-gradle/src/main/java/net/minecraftforge/renamer/gradle/MixinSourceSetConfig.java
+++ renamer-gradle/src/main/java/net/minecraftforge/renamer/gradle/MixinSourceSetConfig.java
@@ -10,16 +10,89 @@ import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
 
 public interface MixinSourceSetConfig {
+    /**
+     * {@inheritDoc}
+     * The reference mapping for this source set.
+     * Will inform Mixin of which symbols are mapped to which names at runtime
+     */
 	Property<String> getRefMap();
+
+    /**
+     * {@inheritDoc}
+     * Stops the Mixin annotation processor from throwing an error when it finds an invalid Mixin target
+     */
 	Property<Boolean> getDisableTargetValidator();
+
+    /**
+     * {@inheritDoc}
+     * Prevents the annotation processor from creating a small {@code .csv} file with data pertaining to annotation processing.
+     * Keeping this on will make the AP run slightly faster, but may be worth turning off if you are getting AP file access errors
+     */
 	Property<Boolean> getDisableTargetExport();
+
+    /**
+     * {@inheritDoc}
+     * By default, when a Mixin target is annotated with {@code @Overwrite}, it is enforced that a Javadoc block above it
+     * contains at least an {@code @author} and {@code @reason} tag. Turning this off will prevent Mixin from complaining
+     * (ignore, warn or error, see {@link #getOverwriteErrorLevel()}) in such a case where these tags are missing
+     */
 	Property<Boolean> getDisableOverwriteChecker();
+
+    /**
+     * {@inheritDoc}
+     * The level of complaining the Mixin annotation processor will do when it finds an {@code @Overwrite} annotated target
+     * lacking the {@code @author} and {@code @reason} tags. Can be any of the following:
+     * <ul>
+     *     <li>{@code ignore}: Do nothing</li>
+     *     <li>{@code warning}: Write a warning message to the console</li>
+     *     <li>{@code error}: Throw an error</li>
+     * </ul>
+     */
 	Property<String> getOverwriteErrorLevel();
+
+    /**
+     * {@inheritDoc}
+     * The current workspace mapping type, used so that Mixin can translate your obfuscated names for creating the refmap
+     */
 	Property<String> getDefaultObfuscationEnv();
+
+    /**
+     * {@inheritDoc}
+     * The other half of {@link #getDefaultObfuscationEnv()}, the output mapping type to translate to for creating entries in the refmap
+     */
     ListProperty<String> getMappingTypes();
+
+    /**
+     * {@inheritDoc}
+     * Extra data passed to the compiler as key-value pairs. These extra data can then be used in annotation parameters.
+     * <p>
+     * Example :{@code @Inject(method = "foo" at = @At("HEAD"), constraints="myToken(myValue)"}
+     */
 	MapProperty<String, String> getTokens();
+
+    /**
+     * {@inheritDoc}
+     * Extra mapping files to use for creating refmaps, usually for obfuscated external dependencies
+     */
 	ConfigurableFileCollection getExtraMappings();
+
+    /**
+     * {@inheritDoc}
+     * "Quietens" the console by suppressing trivial and often unneeded messages
+     */
 	Property<Boolean> getQuiet();
+
+    /**
+     * {@inheritDoc}
+     * Prefix mixin logs with the type of message, such as {@code [MIXIN_0100]} which refers to a missing {@code @author} tag
+     */
 	Property<Boolean> getShowMessageTypes();
+
+    /**
+     * {@inheritDoc}
+     * Which message types to hide, warn about or simply display. These can be seen by enabling {@link #getShowMessageTypes()}
+     * <p>
+     * Example: {@code "MIXIN_0100": "ignore"}
+     */
 	MapProperty<String, String> getMessages();
 }
